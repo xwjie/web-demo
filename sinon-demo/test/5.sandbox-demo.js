3134e03d5dc873b14625277bd9d89d569dc63bee
晓风轻 <121509092@qq.com>
2022-11-09 23:11:28 +0800

    Create 5.sandbox-demo.js

---
+++ sinon-demo/test/5.sandbox-demo.js
@@ -0,0 +1,25 @@
+var sandbox = require("sinon").createSandbox();
+var myAPI = { hello: function () {} };
+
+describe("myAPI.hello method", function () {
+    beforeEach(function () {
+        // stub out the `hello` method
+        sandbox.stub(myAPI, "hello");
+    });
+
+    afterEach(function () {
+        // completely restore all fakes created through the sandbox
+        sandbox.restore();
+    });
+
+    it("should be called once", function () {
+        myAPI.hello();
+        sandbox.assert.calledOnce(myAPI.hello);
+    });
+
+    it("should be called twice", function () {
+        myAPI.hello();
+        myAPI.hello();
+        sandbox.assert.calledTwice(myAPI.hello);
+    });
+});
