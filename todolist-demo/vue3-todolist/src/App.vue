47d641048879996902ea51b047e281103a9ee07a
晓风轻 <121509092@qq.com>
2022-11-20 05:56:41 +0800

    调整布局

--- todolist-demo/vue3-todolist/src/App.vue
+++ todolist-demo/vue3-todolist/src/App.vue
@@ -1,47 +1,54 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+    import HelloWorld from './components/HelloWorld.vue'
+    import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <header>
+        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+        <div class="wrapper">
+            <!--      <HelloWorld msg="You did it!" />-->
+        </div>
+    </header>
+
+    <main>
+        <section class="todoapp">
+            <header class="header">
+                <h1>Todos</h1>
+                <input type="text" class="new-todo" placeholder="想干的事" @keyup.enter="addTodo" v-model="newTodo">
+                <!-- //添加绑定事件 -->
+            </header>
+        </section>
+    </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+    header {
+        line-height: 1.5;
+    }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+    .logo {
+        display: block;
+        margin: 0 auto 2rem;
+    }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    /*@media (min-width: 1024px) {*/
+    /*    header {*/
+    /*        display: flex;*/
+    /*        place-items: center;*/
+    /*        padding-right: calc(var(--section-gap) / 2);*/
+    /*    }*/
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    /*    .logo {*/
+    /*        margin: 0 2rem 0 0;*/
+    /*    }*/
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+    /*    header .wrapper {*/
+    /*        display: flex;*/
+    /*        place-items: flex-start;*/
+    /*        flex-wrap: wrap;*/
+    /*    }*/
+    /*}*/
 </style>
--- todolist-demo/vue3-todolist/src/main.js
+++ todolist-demo/vue3-todolist/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
+import 'todomvc-app-css/index.css'
 
 createApp(App).mount('#app')
