a4265704654ba0272c45cb416769476a43e78b9b
晓风轻 <121509092@qq.com>
2022-11-21 23:37:19 +0800

    tree demo

--- todolist-demo/vue3-todolist/src/App.vue
+++ todolist-demo/vue3-todolist/src/App.vue
@@ -1,54 +1,86 @@
 <script setup>
-    import HelloWorld from './components/HelloWorld.vue'
-    import TheWelcome from './components/TheWelcome.vue'
+    import {reactive, computed} from 'vue'
+
+    const state = reactive({
+        newTodo: '',
+        todos: [
+            {id: '1', title: '吃饭', completed: true},
+            {id: '2', title: '睡觉', completed: false}
+        ]
+    })
+
+    const addTodo = () => {
+        console.log(state.newTodo);
+
+        //去空格
+        let value = state.newTodo && state.newTodo.trim()
+
+        if (!value) return;
+
+        state.todos.push({
+            id: state.todos.length + 1,
+            title: value,
+            completed: false
+        })
+
+        //清空
+        state.newTodo = ''
+    };
+
+    const removeTodo = index => {
+        console.log(index);
+        state.todos.splice(index, 1)
+    };
+
+    // 是否全部完成
+    const allCompleted = computed(() => state.todos.every(item => item.completed));
+
+    // 完成的个数
+    const completedCount = computed(() => state.todos.filter(item => item.completed).length)
 </script>
 
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-
-        <div class="wrapper">
-            <!--      <HelloWorld msg="You did it!" />-->
-        </div>
-    </header>
-
-    <main>
-        <section class="todoapp">
-            <header class="header">
-                <h1>Todos</h1>
-                <input type="text" class="new-todo" placeholder="想干的事" @keyup.enter="addTodo" v-model="newTodo">
-                <!-- //添加绑定事件 -->
-            </header>
+    <section class="todoapp">
+        <header class="header">
+            <h1>todos</h1>
+            <input class="new-todo" placeholder="What needs to be done?" autofocus="" @keyup.enter="addTodo"
+                   v-model="state.newTodo">
+        </header>
+        <section class="main" style="display: block;">
+            <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allCompleted">
+            <label for="toggle-all">Mark all as complete</label>
+            <ul class="todo-list">
+                <li v-for="(item,index) in state.todos" :id="item.id"
+                    :class="{completed: item.completed, editing:item.completed }">
+                    <div class="view">
+                        <input class="toggle" type="checkbox" v-model="item.completed">
+                        <label>{{item.title}}</label>
+                        <button class="destroy" @click="removeTodo(index)"></button>
+                    </div>
+                    <input class="edit" v-model="item.title">
+                </li>
+            </ul>
         </section>
-    </main>
+        <footer class="footer" style="display: block;">
+            <span class="todo-count"><strong>1</strong> item left</span>
+            <ul class="filters">
+                <li>
+                    <a class="selected" href="#/all">All</a>
+                </li>
+                <li>
+                    <a href="#/active">Active</a>
+                </li>
+                <li>
+                    <a href="#/completed">Completed</a>
+                </li>
+            </ul>
+            <button class="clear-completed" v-if="completedCount">Clear completed</button>
+        </footer>
+    </section>
+    <footer class="info">
+        <p>Double-click to edit a todo</p>
+    </footer>
 </template>
 
 <style scoped>
-    header {
-        line-height: 1.5;
-    }
-
-    .logo {
-        display: block;
-        margin: 0 auto 2rem;
-    }
-
-    /*@media (min-width: 1024px) {*/
-    /*    header {*/
-    /*        display: flex;*/
-    /*        place-items: center;*/
-    /*        padding-right: calc(var(--section-gap) / 2);*/
-    /*    }*/
-
-    /*    .logo {*/
-    /*        margin: 0 2rem 0 0;*/
-    /*    }*/
-
-    /*    header .wrapper {*/
-    /*        display: flex;*/
-    /*        place-items: flex-start;*/
-    /*        flex-wrap: wrap;*/
-    /*    }*/
-    /*}*/
 </style>
---
+++ type-script/9.tree-demo.ts
@@ -0,0 +1,44 @@
+interface TreeNode {
+    left?: TreeNode;
+    right?: TreeNode;
+    val: number;
+}
+
+const root: TreeNode = {
+    val : 1 ,
+    left : {
+        val : 2 ,
+        left : {
+            val : 4 ,
+            right : {
+                val : 5
+            }
+        }
+    } ,
+    right : {
+        val : 3
+    }
+};
+
+const treeWalker: (root: TreeNode) => number[] = (root: TreeNode) => {
+    let result: number[] = [];
+
+    // 可选参数必修放前面
+    function _treeWalk(result: number[] , node?: TreeNode) {
+        if (!node) {
+            return
+        }
+
+        result.push(node.val);
+
+        // 递归
+        _treeWalk(result , node.left);
+        _treeWalk(result , node.right);
+    }
+
+    _treeWalk(result , root);
+
+    return result;
+}
+
+console.log(treeWalker(root))
